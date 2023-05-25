c1306472aa10e369c60c804bedd95969a29474c1
VertikaJain <vertikajain96@gmail.com>
2023-05-25 16:51:18 +0530

    Setting up of MongoDB connection using mongoose

--- server/app.js
+++ server/app.js
@@ -2,15 +2,29 @@ import dotenv from "dotenv";
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { MyAppSchema } from "./schema/index.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
+mongoose.connect(`${process.env.MONGODB_URI}${process.env.MONGODB_DB_MAIN}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("connection to db opened");
+});
+
+mongoose.connection.once("error", (error) => {
+  console.log("error in connecting to db: ", error);
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: MyAppSchema,
+    schema: MyAppSchema, // defines schema of the graph and the object types
     graphiql: process.env.NODE_ENV === "development",
   })
 );
